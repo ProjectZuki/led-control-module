f07176296ef51d630ab914e22931558ac0d8b275
Willie Alcaraz <63901327+ProjectZuki@users.noreply.github.com>
2024-05-26 23:12:18 -0700

    Updated IR_button.ino
    Working method to set R,G,B based on IR input, and flash when button is pressed.

--- testLED.ino
+++ testLED.ino
@@ -41,7 +41,6 @@ void loop() {
   delay(10);  // Delay a little bit to improve simulation performance
 }
 
-
 // =============================================================================
 
 // sample code
